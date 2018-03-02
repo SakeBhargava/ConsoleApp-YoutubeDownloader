2d626372516a602cdfca786f4b19b86d138d9cdf
Bhargava Sake <bhargava649@gmail.com>
2018-03-02 08:14:18 +0530

    commit

---
+++ WindowsApp-YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsApp_YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URL = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblResultsText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // URL
+            // 
+            this.URL.AutoSize = true;
+            this.URL.Location = new System.Drawing.Point(33, 43);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(32, 13);
+            this.URL.TabIndex = 0;
+            this.URL.Text = "URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(71, 43);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(266, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(132, 89);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblResultsText
+            // 
+            this.lblResultsText.AutoSize = true;
+            this.lblResultsText.Location = new System.Drawing.Point(213, 99);
+            this.lblResultsText.Name = "lblResultsText";
+            this.lblResultsText.Size = new System.Drawing.Size(0, 13);
+            this.lblResultsText.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 374);
+            this.Controls.Add(this.lblResultsText);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.URL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label URL;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblResultsText;
+    }
+}
+
---
+++ WindowsApp-YoutubeDownloader/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp_YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender1, EventArgs e1)
+        {
+            string url = txtUrl.Text;
+            lblResultsText.Text = "Download progressing...";
+            var processInfo = new ProcessStartInfo("youtube-dl.exe", url);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+           
+            process.BeginOutputReadLine();
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            lblResultsText.Text = $"Download complete and video is available at: {AppDomain.CurrentDomain.BaseDirectory}";
+            process.Close();
+        }
+    }
+}
